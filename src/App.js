d5a62a6ff1a9497a1a071f53b09dfa96b80e269f
Luc <luc.guillemot@gmail.com>
2018-08-08 09:59:18 +0200

    Update questions

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ export class App extends React.Component {
     return (
       <div className="wrapper">
         <h1>Inclusive Data Visualization</h1>
-        <h2>Streamgraph</h2>
+        <h2>How many planets?</h2>
         <Streamgraph />
-        <h2>Scatterplot</h2>
+        <h2>What type of planet for what type of discovery method?</h2>
         <Scatterplot />
       </div>
     );

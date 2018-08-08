f8f0eac3cef772b90c3e960409c394e4ad27ddf3
Luc <luc.guillemot@gmail.com>
2018-08-08 09:20:38 +0200

    add orbital periods to dataset

--- src/data/planets.R
+++ src/data/planets.R
@@ -1,12 +1,7 @@
 library(jsonlite)
 library(tidyverse)
 raw <- read.csv("raw/planets.csv", skip=358)
-d <- raw %>% select(pl_name, pl_disc, pl_discmethod, pl_rade, pl_masse, st_dist)
-
-
-# Data for streamgraph
-
-
+d <- raw %>% select(pl_name, pl_disc, pl_discmethod, pl_rade, pl_masse, st_dist, pl_orbper)
 
 # Export as JSON
 json <- toJSON(d, pretty=T, na="null")

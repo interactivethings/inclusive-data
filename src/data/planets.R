3663d1da91ad80301ccd24ac851390025038ef03
Luc <luc.guillemot@gmail.com>
2018-08-14 10:03:38 +0200

    Add indicators to data file

--- src/data/planets.R
+++ src/data/planets.R
@@ -1,7 +1,7 @@
 library(jsonlite)
 library(tidyverse)
 raw <- read.csv("raw/planets.csv", skip=358)
-d <- raw %>% select(pl_name, pl_disc, pl_discmethod, pl_rade, pl_masse, st_dist, pl_orbper)
+d <- raw %>% select(pl_name, pl_disc, pl_discmethod, pl_rade, pl_masse, st_dist, pl_orbper, st_jmk2, st_hmk2, st_optmag, pl_orbsmax, pl_kepflag, pl_k2flag, st_teff)
 
 # Export as JSON
 json <- toJSON(d, pretty=T, na="null")

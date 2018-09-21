5e0b2c1eb411d19ee8d49d77bf49dc725df46ecd
Luc <luc.guillemot@gmail.com>
2018-09-21 14:38:55 +0200

    embryo of introduction

--- src/Introduction.docs.js
+++ src/Introduction.docs.js
@@ -1,5 +1,15 @@
 import { markdown } from "catalog";
 
 export default () => markdown`
+# What it is
 
+This Catalog is a repository for experimental data representation using not only visual features but also sound, for a multi sensorial immersion into data.
+
+### Inclusivity
+
+The starting point is too make data visualizations accessible to users of assistive technologies such as screen readers. An emphasis is therefore put on keyboard navigation and compatibility with [WAI ARIA](https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA).
+
+### Inspiration
+
+- Brian Foo
 `;

2e48dde7e11729ba9966c3c5ec665ae3054bd3f8
Luc <luc.guillemot@gmail.com>
2018-09-25 09:56:14 +0200

    ts index

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import * as React from "react";
+import * as ReactDOM from "react-dom";
 import { Catalog, pageLoader } from "catalog";
 
 ReactDOM.render(
@@ -33,5 +33,6 @@ ReactDOM.render(
       }
     ]}
   />,
+
   document.getElementById("root")
 );

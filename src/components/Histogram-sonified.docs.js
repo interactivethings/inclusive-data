7ef740f12b24f6fea5326866296f832276a0e9be
Luc <luc.guillemot@gmail.com>
2018-10-12 17:41:59 +0200

    update page title: Sonified > Sonification

--- src/components/Histogram-sonified.docs.js
+++ src/components/Histogram-sonified.docs.js
@@ -3,8 +3,7 @@ import { markdown } from 'catalog';
 import { Histogram } from './Histogram-sonified';
 
 export default () => markdown`
-
-# Histogram - Sonification
+# Sonification
 > Tab to the data visualization, and use arrow keys to navigate between data points. Sound on!
 
 ${<Histogram />}`;
--- src/index.js
+++ src/index.js
@@ -1,37 +1,33 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Catalog, pageLoader } from "catalog";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Catalog, pageLoader } from 'catalog';
 
 ReactDOM.render(
-  <Catalog
-    title=""
-    pages={[
-      {
-        path: "/",
-        title: "Introduction",
-        content: pageLoader(() => import("./Introduction.docs"))
-      },
-      {
-        path: "/histogram",
-        title: "Histogram",
-        pages: [
-          {
-            path: "/histogram/aria",
-            title: "ARIA",
-            content: pageLoader(() =>
-              import("../src/components/Histogram-aria.docs")
-            )
-          },
-          {
-            path: "/histogram/sonified",
-            title: "Sonified",
-            content: pageLoader(() =>
-              import("../src/components/Histogram-sonified.docs")
-            )
-          }
-        ]
-      }
-    ]}
-  />,
-  document.getElementById("root")
+	<Catalog
+		title=""
+		pages={[
+			{
+				path: '/',
+				title: 'Introduction',
+				content: pageLoader(() => import('./Introduction.docs'))
+			},
+			{
+				path: '/histogram',
+				title: 'Histogram',
+				pages: [
+					{
+						path: '/histogram/aria',
+						title: 'ARIA',
+						content: pageLoader(() => import('../src/components/Histogram-aria.docs'))
+					},
+					{
+						path: '/histogram/sonification',
+						title: 'Sonification',
+						content: pageLoader(() => import('../src/components/Histogram-sonified.docs'))
+					}
+				]
+			}
+		]}
+	/>,
+	document.getElementById('root')
 );
